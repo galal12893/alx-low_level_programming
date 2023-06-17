f89d000bf567841900d6720261f12cb8f2416b21
Galal <galal.abouelella@outlook.com>
2023-06-16 18:11:28 -0700

    C :D

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - prints a quote
+ * Return: always 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quote,59);
+	return (1);
