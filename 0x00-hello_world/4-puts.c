2256d9f4cc39a1844dbfedf4007319e90c161bcb
Galal <galal.abouelella@outlook.com>
2023-06-16 17:24:28 -0700

    C :D

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - the main function prints a string
  *
- * Return: always 0 
+ * Return: always 0
 */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
