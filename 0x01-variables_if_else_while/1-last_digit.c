63d50bde355731b5abaee7cc9c84891f4f4af7ae
Galal <galal.abouelella@outlook.com>
2023-06-19 15:28:34 -0700

    playing with flow

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
@@ -9,26 +9,27 @@
 
 int main(void)
 {
-	int n;
-        int lastDigit;
+    int n;
+    int lastDigit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        lastDigit = n % 10;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    lastDigit = n % 10;
 
-      printf("Last digit of %d is %d and is ", n, lastDigit);
-      if (lastDigit > 5)
-      {
-            printf("greater than 5\n");
-      }
-      else if (lastDigit == 0)
-      {
-            printf("0\n");
-      }
-      else
-      {
-            printf("less than 6 and not 0\n");
-      }
+    printf("Last digit of %d is %d and is ", n, lastDigit);
+    if (lastDigit > 5)
+    {
+        printf("greater than 5\n");
+    }
+    else if (lastDigit == 0)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        printf("less than 6 and not 0\n");
+    }
 
-	return (0);
+    return (0);
 }
+
