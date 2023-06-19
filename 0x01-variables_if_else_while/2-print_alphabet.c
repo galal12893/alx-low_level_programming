2e5303ef99e10e0bd06e2b24b007608683d89ab4
Galal <galal.abouelella@outlook.com>
2023-06-19 15:46:29 -0700

    playing with flow

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,14 @@
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
 	char ch;
-	for (ch = 'a';ch <= 'z';ch++)
+	for(ch = 'a';ch <= 'z';ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
 	
-	return 0;	
+	return (0);
 }
