0d8e5b1d738529a01d86e3459219d67dec7e7fcf
Galal <galal.abouelella@outlook.com>
2023-06-21 15:12:56 -0700

    nesting

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * main - prints_putchar
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ * _putcahr - writes the character c to the stdout
+ * @c: the char to print
+ *
+ * Return: on sucess 1.
+ *	   on error, -1 is returned and errno is set approprialety
+ */
+
+int _putcahr(char c)
+{
+	return (write(1, &c, 1));
+}
