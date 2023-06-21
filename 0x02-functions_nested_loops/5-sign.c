fda84a4c1c431bd9f2effd480d0beb4a85aa706d
Galal <galal.abouelella@outlook.com>
2023-06-21 16:15:22 -0700

    nested 10x

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of int
  * @n: the int to be checked
- * Return 1 if + and 0 if -
+ * Return: 1 if + and 0 if -
  */
 
 int print_sign(int n)
@@ -13,7 +13,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n ==0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
