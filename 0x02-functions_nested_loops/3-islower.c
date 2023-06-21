bc9e5cf2609d4321189327fa26baad309ac12fcd
Galal <galal.abouelella@outlook.com>
2023-06-21 16:01:05 -0700

    nested 10x

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,8 @@
 
 /**
  * _islower - checks if char is lowercase
- *
- * Return: 1 if lowercase.
- * 	   0 if upper case
+ *@c: the char to be checked
+ * Return: 1 if lowercase, 0 if upper case
  */
 
 int _islower(int c)
