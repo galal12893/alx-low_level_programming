1ee575510b860b25770ebd7e8b8248618f7364f5
Galal <galal.abouelella@outlook.com>
2023-06-23 19:54:53 -0700

    more

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check if c is upper
+ *
+ * @a: input for digit
+ * @b: input for digit
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
