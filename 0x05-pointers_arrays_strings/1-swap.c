dbc14a25d797b9e749b67486fe7f182e5b30ea62
Galal <galal.abouelella@outlook.com>
2023-06-27 15:39:25 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * @a: pointer to int a
+ * @b: pointer to int b
+ * Return: norhing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *temp;
+
+	*temp = *a;
+	*a = *b;
+	*b = *temp;
+}
