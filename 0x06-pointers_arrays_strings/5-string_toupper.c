55a5350cc683d612c9a621be76145b1c3af540c6
Galal <galal.abouelella@outlook.com>
2023-06-29 21:41:49 +0300

    more pointers from my pc

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @str: adress to the string to change
+ *
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+
+}
