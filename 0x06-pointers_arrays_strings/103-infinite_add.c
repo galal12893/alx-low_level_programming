641b159751fb296d5d02a2b951767cbe7991ab94
Galal <galal.abouelella@outlook.com>
2023-07-01 00:43:00 +0300

    more pointers from my pc

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,38 +11,45 @@
  * Return: 0 or the sum
  */
 
+	char *infinite_add(char *n1, char *n2, char *r, int size_r)
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i = 0, j = 0, k = 0, l = 0, f = 0, s = 0, d = 0;
-	int n1_len = strlen(n1);
-	int n2_len = strlen(n2);
 
-	l = n1_len > n2_len ? n1_len : n2_len;
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
 	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1; k >= 0; k++)
 	{
-		return (NULL);
-	}
-
-	for (k = l - 1; k >= 0; k--)
-	{
-		i = n1_len - 1 - k;
-		j = n2_len - 1 - k;
-		f = i >= 0 ? n1[i] - '0' : 0;
-		s = j >= 0 ? n2[j] - '0' : 0;
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
 		r[k] = (f + s + d) % 10 + '0';
 		d = (f + s + d) / 10;
 	}
-
 	if (d == 1)
 	{
-		r[l] = '1';
-		l++;
-		if (l + 1 > size_r)
-		{
-			return (NULL);
-		}
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
-
-	r[l] = '\0';
 	return (r);
 }
