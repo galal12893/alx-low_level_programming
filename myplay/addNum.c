1a04fec4ba1605a531aad080676bf52267395423
Galal <galal.abouelella@outlook.com>
2023-07-11 03:15:41 +0300

    static lib

---
+++ myplay/addNum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int addNum(int a, int b)
+{
+	printf("a is: %d", a);
+	printf("b is: %d", b);
+	return(a + b);
+}
---
+++ myplay/main.c
@@ -0,0 +1,12 @@
+#include "head.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5;
+	int b = 6;
+	int c;
+	
+	c = addNum(a, b);
+	printf("%d", c);
+}
