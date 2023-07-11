b002612f4ba0a2e1c295cd7a73be2e010ca141c3
Galal <galal.abouelella@outlook.com>
2023-07-12 01:25:18 +0300

    malloc now

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, and initializes it.
+ * create_array - creates an array of chars and initializes it.
  *
- * @size: size of array
- * @c: the char to put in
+ * @size: size of the array
+ * @c: the character to put in
  *
- * Return: Null or pointer to array
+ * Return: pointer to the array or NULL on failure
  */
 
 char *create_array(unsigned int size, char c)
@@ -15,18 +15,21 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	unsigned int i;
 
-	/*Returns NULL if size = 0*/
+	/* Returns NULL if size = 0 */
 	if (size == 0)
 		return (NULL);
 
-	ar = (char *)malloc(size * sizeof(char));
+	ar = malloc(size * sizeof(char));
+
+	if (ar == NULL)
+	{
+		/* Handle failed memory allocation */
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 
-	if (ar[0] == c)
-		return (ar);
-	else
-		return (NULL);
-
+	return (ar);
 }
+
