f79cd42b5305b74b26a5e1ba82f306f4a7f4ecc1
Galal <galal.abouelella@outlook.com>
2023-07-12 02:12:29 +0300

    malloc now

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *
+ * @str: old array
+ * Return: Null or pointer to new array
+ */
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int i, j;
+
+	/*Returns NULL if str = NULL*/
+	if (str == NULL)
+		return (NULL);
+	/*Find length of str*/
+	for (j = 0; str[j] != '\0'; j++)
+		;
+	/*allocate memory for new array*/
+	ar = (char *)malloc(j * sizeof(char));
+	/*check malloc faliure*/
+	if (ar == NULL)
+		return (NULL);
+	/*assign str to new array*/
+	for (i = 0; i < j; i++)
+		ar[i] = str[i];
+
+	return (ar);
+}
