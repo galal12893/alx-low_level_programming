c22314f00221ffa0d690c88d6f079a3a9692e899
Galal <galal.abouelella@outlook.com>
2023-07-11 11:35:23 +0300

    static lib

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line
+ *
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
