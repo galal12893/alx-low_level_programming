762702d973025726f045a77524fa75ce1ec59f11
Galal <galal.abouelella@outlook.com>
2023-07-12 03:37:01 +0300

    malloc now

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - concatenates two strings.
+ *
+ * @width: first string
+ * @height: second string
+ *
+ * Return: pointer to 2d array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
