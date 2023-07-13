ebf668612911a51db81bf03a465d5e1c30c58e2d
Galal <galal.abouelella@outlook.com>
2023-07-14 02:20:27 +0300

    more malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * main - multiplies 2 numbers
+ *
+ * @argc: count
+ * @argv: array of arguments
+ *
+ * Return: result
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
