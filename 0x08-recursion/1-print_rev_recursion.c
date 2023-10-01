e6c8c23cb44a83ca8b8297f1bc01f4e88fb43186
Galal <galal.abouelella@outlook.com>
2023-10-02 01:08:52 +0300

    recursive

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  prints a string, followed by a new line
+ * in reverse order.
+ * @s: string to print
+ *
+ * this function prints a string followed by a new line
+ *
+ * return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+
+	_putchar(*s);
+}	
