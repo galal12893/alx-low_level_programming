dd22d31b221f6714d25edd15c9eb0cd728039f67
Galal <galal.abouelella@outlook.com>
2024-01-20 13:53:09 +0200

    fixed betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,6 +8,9 @@
  * If `i * i` is greater than `n`, -1 is returned.
  * Return: the natural square root of `n`, or -1 if it doesn't exist
  */
+
+
+
 int sqrt_check(int n, int i)
 {
 	if (i * i > n)
@@ -17,6 +20,15 @@ int sqrt_check(int n, int i)
 	return (sqrt_check(n, i + 1));
 }
 
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: the number to calculate the square root of
+ *
+ * Description: This function finds the natural square root of `n`.
+ * It uses a helper function `sqrt_check` to perform the calculation.
+ * If `n` is less than 0, -1 is returned.
+ * Return: the natural square root of `n`, or -1 if it doesn't exist
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
