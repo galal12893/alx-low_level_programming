a3044718eb8b311b3e1253ccf1aa8eb9bae2360b
Galal <galal.abouelella@outlook.com>
2024-01-20 22:23:00 +0200

    added prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,37 +1,32 @@
 #include "main.h"
-/**
- * is_prime_number - checks if int is prime
- *
- * @n: the number to check
- *
- * Return: 1 if prime 0 if not
- */
 
+/**
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: (1) if n is a prime number, (0) otherwise.
+ */
 int is_prime_number(int n)
 {
-	int i = 0;
+	int i;
+
 	if (n <= 1)
-	{
 		return (0);
-	}
 
 	if (n <= 3)
-	{
 		return (1);
-	}
 
 	if (n % 2 == 0 || n % 3 == 0)
-	{
 		return (0);
-	}
 
-	for (i = 5; i * i <= n; i += 6)
+	i = 5;
+	while (i * i <= n)
 	{
 		if (n % i == 0 || n % (i + 2) == 0)
-		{
 			return (0);
-		}
+		i += 6;
 	}
 
-	return 1;
+	return (1);
 }
+
