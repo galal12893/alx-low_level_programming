4cf13c1f3fd3951f373ea5e1926943e51ea02a93
Galal <galal.abouelella@outlook.com>
2024-01-20 12:50:48 +0200

    added factorial file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - returns the factorial of a given number
+ * @n: digit to find the factorial of
+ *
+ * Return: the factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
+
