6936a8485fba92ede61defdda9ef3de6e9f7717e
Galal <galal.abouelella@outlook.com>
2024-01-20 22:26:04 +0200

    added prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * is_prime_number - Check if a number is prime.
+ * is_prime_recursive - Check if a number is prime recursively.
  * @n: The number to be checked.
+ * @divisor: The current divisor being tested.
  *
  * Return: (1) if n is a prime number, (0) otherwise.
  */
-int is_prime_number(int n)
+int is_prime_recursive(int n, int divisor)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
 
@@ -19,14 +18,23 @@ int is_prime_number(int n)
 	if (n % 2 == 0 || n % 3 == 0)
 		return (0);
 
-	i = 5;
-	while (i * i <= n)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-		i += 6;
-	}
+	if (divisor * divisor > n)
+		return (1);
 
-	return (1);
+	if (n % divisor == 0 || n % (divisor + 2) == 0)
+		return (0);
+
+	return is_prime_recursive(n, divisor + 6);
+}
+
+/**
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: (1) if n is a prime number, (0) otherwise.
+ */
+int is_prime_number(int n)
+{
+	return is_prime_recursive(n, 5);
 }
 
