a506d824fb53b00d306076100de7c8ffae23711f
Galal <galal.abouelella@outlook.com>
2024-01-20 22:38:28 +0200

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int check_pal(char *s, int start, int end)
 		return (1);
 	if (s[start] != s[end])
 		return (0);
-	return check_pal(s, start + 1, end - 1);
+	return (check_pal(s, start + 1, end - 1));
 }
 
 /**
@@ -36,6 +36,6 @@ int _strlen_recursion(char *s)
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
-	return check_pal(s, 0, len - 1);
+	return (check_pal(s, 0, len - 1));
 }
 
